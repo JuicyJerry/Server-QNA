f90b0562338ad2539f336c07d779c7f14511a24d
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2025-03-14 18:33:24 +0900

    fixed test code

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -11,7 +11,7 @@ const REDIS_URL = "redis://default:test_env@localhost:6380";
 // 테스트 코드에서는 process.env.REDIS_URL을 사용하지 않음 : 실수로 테스트 코드 돌렸다가 prod나 local의 db 날림을 방지하기 위해
 
 beforeAll(async () => {
-  console.log("List Key: ", LIST_KEY);
+  console.log("[beforeAll]List Key: ", LIST_KEY);
   client = redis.createClient({ url: REDIS_URL });
   await client.connect();
   app = createApp(client);
@@ -33,7 +33,9 @@ describe("POST", () => {
 
   describe("GET", () => {
     it("respond with all messages", async () => {
+      console.log("[GET]List Key: ", LIST_KEY);
       if (LIST_KEY) {
+        console.log("[GET]List Key2 : ", LIST_KEY);
         await client.lPush(LIST_KEY, ["msg1", "msg2"]);
       } else {
         throw new Error("LIST_KEY is not defined");
